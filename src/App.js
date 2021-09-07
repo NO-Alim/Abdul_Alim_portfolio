8d40a419ed61e919ca869c826c6cc5d8f9cd7dde
Abdul Alim <abdulalimrakib53@gmail.com>
2021-09-07 23:30:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Navbar from "./Component/Navbar";
 import Home from "./Pages/Home";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Error from "./Pages/Error";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </switch>
+      </Router>
     </div>
   );
 }
